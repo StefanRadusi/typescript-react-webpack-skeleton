8a6042f2675481afb128489becfcc94bfa3ae596
Stefan Radusi <radusi.stefan@gmail.com>
2018-05-03 09:09:42 +0300

    prime

---
+++ dist/59353d0b9d34611c2570.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{14:function(e,n,t){"use strict";t.r(n);var o=t(0),r=t(5);r.render(o.createElement("p",null," Test "),document.getElementById("root"))}},[[14,0,1]]]);
---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as style from './style.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <p> this is the app </p>
+        )
+    }
+}
+
+export default App;
